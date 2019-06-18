ee16cf8fc3ca6719e75a2df0fe9570c7a52b59e2
gaowei <gaowei@fengjinggroup.com>
2019-06-18 12:38:40 +0800

    sonar

--- vars/pipeline_java.groovy
+++ vars/pipeline_java.groovy
@@ -56,7 +56,7 @@ def call(Map map) {
                 steps {
                     script {
                         def sonarHome = tool name: 'SonarQube Scanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
-                        withSonarQubeEnv(SonarQube Scanner){
+                        withSonarQubeEnv('SonarQube Scanner'){
                             sh "${sonarHome}/bin/sonar-scanner -Dsonar.host.url=http://sonar.top.mw"
                         }
                     }
