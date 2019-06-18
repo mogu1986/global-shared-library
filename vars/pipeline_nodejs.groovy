a23804b96dd7721fc680dd36ffd54a1b8d062f35
gaowei <gaowei@fengjinggroup.com>
2019-06-18 17:15:34 +0800

    zip

--- vars/pipeline_nodejs.groovy
+++ vars/pipeline_nodejs.groovy
@@ -56,6 +56,7 @@ def call(Map map) {
                 steps {
                     script {
                         zip zipFile: 'Test.zip', dir: "dist"
+                        sh 'ls -la'
                     }
                 }
             }
