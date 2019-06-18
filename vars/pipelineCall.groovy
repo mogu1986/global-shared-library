01155004602528341e2ad333b57e544b991e3ac1
gaowei <gaowei@fengjinggroup.com>
2019-06-18 13:30:40 +0800

    sonar

--- vars/pipelineCall.groovy
+++ vars/pipelineCall.groovy
@@ -46,7 +46,7 @@ def putSonarKey(Map params, String app) {
     params.put('vue', '2632d5e4376ad7aa300fd70e3b8254504573a294')
     params.put('distribution', '9fd1c8b5372f57a5487355c43b91ad956a8cd25c')
 
-    if (params.containsKey(app)) {
+    if (map.containsKey(app)) {
         def key = map.get(app)
         params.put('sonar_login', key)
         log.debug("sonar_login = ${key}")
