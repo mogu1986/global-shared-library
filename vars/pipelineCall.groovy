0e9fe654c235a1a568b368db9de63f25604af372
mogu1986 <session.blue@gmail.com>
2019-12-11 19:39:40 +0800

    Update pipelineCall.groovy

--- vars/pipelineCall.groovy
+++ vars/pipelineCall.groovy
@@ -1,38 +1,83 @@
-def call(Map params) {
+def call(String app="${JOB_NAME}", String lang = "${JOB_NAME}") {
 
-    if (params == null || params.size() == 0) {
-        log.error("error : params is null")
-        return
+    log.debug("app = ${app}")
+
+    def params = new com.sxh.AppMeta().getMeta(app)
+    
+    if (params != null) {
+        lang = params.get('lang')
+        log.debug("${app} 查找结果 : ${params}")
     }
 
-    def keys = ['lang', 'app', 'artifact']
-
-    for (int i = 0; i < keys.size(); i++) {
-        def key = keys.get(i)
-        if (!params.containsKey(key)) {
-            log.error("error : key ${key} is null")
-            return
-        }
-    }
-
-    def lang = params.get('lang')
-    def app = params.get('app')
-    def artifact = params.get('artifact')
-
-    log.debug("print : lang = ${lang}, app = ${app}, artifact = ${artifact}")
-
-    putSonar(params)
+    log.debug("lang : ${lang}")
 
     switch (lang) {
+        case "jar":
+            pipeline_jar(params)
+            break
         case "java":
             pipeline_java(params)
-            break;
+            break
         case "tomcat":
             pipeline_tomcat(params)
-            break;
+            break
         case "nodejs":
             pipeline_nodejs(params)
-            break;
+            break
+        case "html":
+            pipeline_html(params)
+            break
+        case "aliyun-harbor":
+            pipeline_harbor(params)
+            break
+        case "nodejsyqm":
+            pipeline_nodejsyqm(params)
+            break
+        case "wx":
+            pipeline_wx(params)
+            break
+        case "h5web":
+            pipeline_h5web(params)
+            break
+        case "release":
+            pipeline_release(params)
+            break
+        case "release_k8s":
+            pipeline_release_k8s(params)
+            break
+        case "aliyun-sync-war":
+            pipeline_sync_war(params)
+            break
+        case "aliyun-sync-war-wx":
+            pipeline_sync_war_wx(params)
+            break
+        case "aliyun-sync-war-h5web":
+            pipeline_sync_war_h5web(params)
+            break
+        case "aliyun-harbor-nodejs":
+            pipeline_sync_nodejs(params)
+            break
+        case "aliyun-harbor-html":
+            pipeline_sync_html(params)
+            break
+        case "aliyun2qcloud":
+            pipeline_aliyun2qcloud(params)
+            break
+        case "qcloud-sync-war":
+            pipeline_qcloud_sync_war(params)
+            break
+        case "qcloud-sync-war-wx":
+            pipeline_qcloud_sync_war_wx(params)
+            break
+        case "qcloud-sync-war-h5web":
+            pipeline_qcloud_sync_war_h5web(params)
+            break
+        case "qcloud-release":
+            pipeline_qcloud_release(params)
+            break
+        case "qcloud_release_k8s":
+            pipeline_qcloud_release_k8s(params)
+            break            
         default:
             println "nice to meet you"
     }
