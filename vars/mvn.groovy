da0f85e28acc7160a0382cac49fc673ccc67777e
gaowei <1qaz2wsx>
2019-12-13 13:51:39 +0800

    auth

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -1,9 +1,9 @@
 def call(mvnExec) {
     configFileProvider(
-            [configFile(fileId: "maven-global-settings", variable: 'MAVEN_SETTINGS_ENV')]) {
-            docker.image('maven:3-jdk-8-alpine').inside('-v /root/.m2:/root/.m2') {
-                log.debug("MAVEN_SETTINGS: ${MAVEN_SETTINGS_ENV}")
-                mvnExec("${MAVEN_SETTINGS_ENV}")
-            }
+        [configFile(fileId: "maven-global-settings", variable: 'MAVEN_SETTINGS_ENV')]) {
+        docker.image('maven:3-jdk-8-alpine').inside('-v /root/.m2:/root/.m2') {
+            log.debug("MAVEN_SETTINGS: ${MAVEN_SETTINGS_ENV}")
+            mvnExec("${MAVEN_SETTINGS_ENV}")
+        }
     }
 }
